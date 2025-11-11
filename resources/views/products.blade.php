8793f57cdbcc1e18d8fa3425987597a2cadd22ec
insolutes2023 <testinsolutes@gmail.com>
2025-11-11 17:01:36 +0530

    files

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -3,4 +3,135 @@
 
 @section('content')
 
+<!--Page Header Start-->
+<section class="page-header">
+    <div class="page-header-bg" style="background-image: url(assets/images/backgrounds/page-header-bg.jpg)">
+    </div>
+
+    <div class="container">
+        <div class="page-header__inner">
+            <h2>Projects-3</h2>
+            <div class="thm-breadcrumb__box">
+                <ul class="thm-breadcrumb list-unstyled">
+                    <li><a href="index.html">Home</a></li>
+                    <li><span>/</span></li>
+                    <li>Projects</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+<!--Page Header End-->
+
+<!--Project Page Three Start-->
+<section class="project-page-three">
+    <div class="container">
+        <div class="row">
+            <!--Project Four Single Start-->
+            <div class="col-xl-4 col-lg-6 col-md-6">
+                <div class="project-four__single">
+                    <div class="project-four__img">
+                        <img src="assets/images/project/project-page-3-1.jpg" alt="">
+                        <div class="project-four__content">
+                            <p class="project-four__sub-title">Recent Project</p>
+                            <h3 class="project-four__title"><a href="project-details.html">Russian
+                                    Pertroleum</a></h3>
+                        </div>
+                        <div class="project-four__arrow">
+                            <a href="project-details.html"><span class="icon-right-arrow"></span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--Project Four Single End-->
+            <!--Project Four Single Start-->
+            <div class="col-xl-4 col-lg-6 col-md-6">
+                <div class="project-four__single">
+                    <div class="project-four__img">
+                        <img src="assets/images/project/project-page-3-2.jpg" alt="">
+                        <div class="project-four__content">
+                            <p class="project-four__sub-title">Recent Project</p>
+                            <h3 class="project-four__title"><a href="project-details.html">Oil Refinery
+                                    Works</a></h3>
+                        </div>
+                        <div class="project-four__arrow">
+                            <a href="project-details.html"><span class="icon-right-arrow"></span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--Project Four Single End-->
+            <!--Project Four Single Start-->
+            <div class="col-xl-4 col-lg-6 col-md-6">
+                <div class="project-four__single">
+                    <div class="project-four__img">
+                        <img src="assets/images/project/project-page-3-3.jpg" alt="">
+                        <div class="project-four__content">
+                            <p class="project-four__sub-title">Recent Project</p>
+                            <h3 class="project-four__title"><a href="project-details.html">Petrol & Energy</a>
+                            </h3>
+                        </div>
+                        <div class="project-four__arrow">
+                            <a href="project-details.html"><span class="icon-right-arrow"></span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--Project Four Single End-->
+            <!--Project Four Single Start-->
+            <div class="col-xl-4 col-lg-6 col-md-6">
+                <div class="project-four__single">
+                    <div class="project-four__img">
+                        <img src="assets/images/project/project-page-3-4.jpg" alt="">
+                        <div class="project-four__content">
+                            <p class="project-four__sub-title">Recent Project</p>
+                            <h3 class="project-four__title"><a href="project-details.html">Gas Industries</a>
+                            </h3>
+                        </div>
+                        <div class="project-four__arrow">
+                            <a href="project-details.html"><span class="icon-right-arrow"></span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--Project Four Single End-->
+            <!--Project Four Single Start-->
+            <div class="col-xl-4 col-lg-6 col-md-6">
+                <div class="project-four__single">
+                    <div class="project-four__img">
+                        <img src="assets/images/project/project-page-3-5.jpg" alt="">
+                        <div class="project-four__content">
+                            <p class="project-four__sub-title">Recent Project</p>
+                            <h3 class="project-four__title"><a href="project-details.html">Petrol Research</a>
+                            </h3>
+                        </div>
+                        <div class="project-four__arrow">
+                            <a href="project-details.html"><span class="icon-right-arrow"></span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--Project Four Single End-->
+            <!--Project Four Single Start-->
+            <div class="col-xl-4 col-lg-6 col-md-6">
+                <div class="project-four__single">
+                    <div class="project-four__img">
+                        <img src="assets/images/project/project-page-3-6.jpg" alt="">
+                        <div class="project-four__content">
+                            <p class="project-four__sub-title">Recent Project</p>
+                            <h3 class="project-four__title"><a href="project-details.html">Manufacturing</a>
+                            </h3>
+                        </div>
+                        <div class="project-four__arrow">
+                            <a href="project-details.html"><span class="icon-right-arrow"></span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--Project Four Single End-->
+        </div>
+    </div>
+</section>
+<!--Project Page Three End-->
+
 @endsection
