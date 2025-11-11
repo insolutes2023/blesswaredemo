fd018e4eaff0b06481d208ca1840041e463aeea7
insolutes2023 <testinsolutes@gmail.com>
2025-11-11 16:53:24 +0530

    files

--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -3,4 +3,168 @@
 
 @section('content')
 
+<!--Page Header Start-->
+<section class="page-header">
+    <div class="page-header-bg" style="background-image: url(assets/images/backgrounds/page-header-bg.jpg)">
+    </div>
+
+    <div class="container">
+        <div class="page-header__inner">
+            <h2>Services-5</h2>
+            <div class="thm-breadcrumb__box">
+                <ul class="thm-breadcrumb list-unstyled">
+                    <li><a href="index.html">Home</a></li>
+                    <li><span>/</span></li>
+                    <li>Services</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+<!--Page Header End-->
+
+<!--Services Page Five Start-->
+<section class="services-page-five">
+    <div class="container">
+        <div class="row">
+            <!--Services Five Single Start-->
+            <div class="col-xl-4 col-lg-4 col-md-6">
+                <div class="services-five__single">
+                    <div class="services-five__img">
+                        <img src="assets/images/services/services-5-1.jpg" alt="">
+                    </div>
+                    <div class="services-five__content-box">
+                        <div class="services-five__icon">
+                            <span class="icon-gas"></span>
+                        </div>
+                        <div class="services-five__content">
+                            <h3 class="services-five__title"><a href="services-5.html">Medical industry</a></h3>
+                            <p class="services-five__text">Aliq is notm hendr erit a augue insu image pellen
+                                simply
+                                free text.</p>
+                            <a href="services-5.html" class="services-five__btn">read more <span
+                                    class="icon-right-arrow"></span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--Services Five Single End-->
+            <!--Services Five Single Start-->
+            <div class="col-xl-4 col-lg-4 col-md-6">
+                <div class="services-five__single">
+                    <div class="services-five__img">
+                        <img src="assets/images/services/services-5-2.jpg" alt="">
+                    </div>
+                    <div class="services-five__content-box">
+                        <div class="services-five__icon">
+                            <span class="icon-gas"></span>
+                        </div>
+                        <div class="services-five__content">
+                            <h3 class="services-five__title"><a href="services-5.html">Medical industry</a></h3>
+                            <p class="services-five__text">Aliq is notm hendr erit a augue insu image pellen
+                                simply
+                                free text.</p>
+                            <a href="services-5.html" class="services-five__btn">read more <span
+                                    class="icon-right-arrow"></span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--Services Five Single End-->
+            <!--Services Five Single Start-->
+            <div class="col-xl-4 col-lg-4 col-md-6">
+                <div class="services-five__single">
+                    <div class="services-five__img">
+                        <img src="assets/images/services/services-5-3.jpg" alt="">
+                    </div>
+                    <div class="services-five__content-box">
+                        <div class="services-five__icon">
+                            <span class="icon-gas"></span>
+                        </div>
+                        <div class="services-five__content">
+                            <h3 class="services-five__title"><a href="services-5.html">Medical industry</a></h3>
+                            <p class="services-five__text">Aliq is notm hendr erit a augue insu image pellen
+                                simply
+                                free text.</p>
+                            <a href="services-5.html" class="services-five__btn">read more <span
+                                    class="icon-right-arrow"></span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--Services Five Single End-->
+            <!--Services Five Single Start-->
+            <div class="col-xl-4 col-lg-4 col-md-6">
+                <div class="services-five__single">
+                    <div class="services-five__img">
+                        <img src="assets/images/services/services-5-4.jpg" alt="">
+                    </div>
+                    <div class="services-five__content-box">
+                        <div class="services-five__icon">
+                            <span class="icon-biotechnology"></span>
+                        </div>
+                        <div class="services-five__content">
+                            <h3 class="services-five__title"><a href="services-5.html">Biotechnology</a></h3>
+                            <p class="services-five__text">Aliq is notm hendr erit a augue insu image pellen
+                                simply
+                                free text.</p>
+                            <a href="services-5.html" class="services-five__btn">read more <span
+                                    class="icon-right-arrow"></span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--Services Five Single End-->
+            <!--Services Five Single Start-->
+            <div class="col-xl-4 col-lg-4 col-md-6">
+                <div class="services-five__single">
+                    <div class="services-five__img">
+                        <img src="assets/images/services/services-5-5.jpg" alt="">
+                    </div>
+                    <div class="services-five__content-box">
+                        <div class="services-five__icon">
+                            <span class="icon-production"></span>
+                        </div>
+                        <div class="services-five__content">
+                            <h3 class="services-five__title"><a href="services-5.html">Productions</a></h3>
+                            <p class="services-five__text">Aliq is notm hendr erit a augue insu image pellen
+                                simply
+                                free text.</p>
+                            <a href="services-5.html" class="services-five__btn">read more <span
+                                    class="icon-right-arrow"></span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--Services Five Single End-->
+            <!--Services Five Single Start-->
+            <div class="col-xl-4 col-lg-4 col-md-6">
+                <div class="services-five__single">
+                    <div class="services-five__img">
+                        <img src="assets/images/services/services-5-6.jpg" alt="">
+                    </div>
+                    <div class="services-five__content-box">
+                        <div class="services-five__icon">
+                            <span class="icon-respiratory"></span>
+                        </div>
+                        <div class="services-five__content">
+                            <h3 class="services-five__title"><a href="services-5.html">Respiratory</a></h3>
+                            <p class="services-five__text">Aliq is notm hendr erit a augue insu image pellen
+                                simply
+                                free text.</p>
+                            <a href="services-5.html" class="services-five__btn">read more <span
+                                    class="icon-right-arrow"></span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--Services Five Single End-->
+        </div>
+    </div>
+</section>
+<!--Services Page Five End-->
+
+
+
+
 @endsection
