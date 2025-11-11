6f48ff924bf8e51fd2c53cc8e78f209c38deed15
insolutes2023 <testinsolutes@gmail.com>
2025-11-11 14:45:28 +0530

    files

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -3,6 +3,135 @@
 
 @section('content')
 
+<!--Page Header Start-->
+<section class="page-header">
+    <div class="page-header-bg" style="background-image: url(assets/images/backgrounds/page-header-bg.jpg)">
+    </div>
+
+    <div class="container">
+        <div class="page-header__inner">
+            <h2>Contact</h2>
+            <div class="thm-breadcrumb__box">
+                <ul class="thm-breadcrumb list-unstyled">
+                    <li><a href="index.html">Home</a></li>
+                    <li><span>/</span></li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+<!--Page Header End-->
+
+<!--Contact Info Start-->
+<section class="contact-info">
+    <div class="container">
+        <div class="row">
+            <!--Contact Info Single Start-->
+            <div class="col-xl-4 col-lg-4 wow fadeInUp" data-wow-delay="100ms">
+                <div class="contact-info__single">
+                    <h4 class="contact-info__title">About</h4>
+                    <p class="contact-info__text">Lorem ipsum is simply free text not <br> used by copytyping
+                        refreshing. <br> Neque porro est qui</p>
+                    <div class="contact-info__btn-box">
+                        <div class="contact-info__btn">
+                            <a href="contact.html">Contact</a>
+                        </div>
+                    </div>
+                    <div class="contact-info__icon">
+                        <span class="icon-entrepreneur"></span>
+                    </div>
+                </div>
+            </div>
+            <!--Contact Info Single End-->
+            <!--Contact Info Single Start-->
+            <div class="col-xl-4 col-lg-4 wow fadeInUp" data-wow-delay="200ms">
+                <div class="contact-info__single">
+                    <h4 class="contact-info__title">Contact</h4>
+                    <p class="contact-info__text">Mon-Sat 8:00 - 6:30 Sunday Closed <br> <a
+                            href="mailto:needhelp@company.com">needhelp@company.com</a> <br> <a
+                            href="tel:922460088">+ 92 (246) 0088</a></p>
+                    <div class="contact-info__btn-box">
+                        <div class="contact-info__btn">
+                            <a href="contact.html">Contact</a>
+                        </div>
+                    </div>
+                    <div class="contact-info__icon">
+                        <span class="icon-contact"></span>
+                    </div>
+                </div>
+            </div>
+            <!--Contact Info Single End-->
+            <!--Contact Info Single Start-->
+            <div class="col-xl-4 col-lg-4 wow fadeInUp" data-wow-delay="300ms">
+                <div class="contact-info__single">
+                    <h4 class="contact-info__title">Address</h4>
+                    <p class="contact-info__text">80 Broklyn Golden Seven Street, <br> New York. United States
+                        of <br> America </p>
+                    <div class="contact-info__btn-box">
+                        <div class="contact-info__btn">
+                            <a href="contact.html">Contact</a>
+                        </div>
+                    </div>
+                    <div class="contact-info__icon">
+                        <span class="icon-location"></span>
+                    </div>
+                </div>
+            </div>
+            <!--Contact Info Single End-->
+        </div>
+    </div>
+</section>
+<!--Contact Info End-->
+
+<!--Contact Page Form Start-->
+<section class="contact-page-form">
+    <div class="contact-page-form__shape-1 float-bob-x">
+        <img src="assets/images/shapes/contact-page-form-shape-1.png" alt="">
+    </div>
+    <div class="container">
+        <div class="section-title text-center">
+            <span class="section-title__tagline">Contact With Us</span>
+            <h2 class="section-title__title">Feel free to write our <br> team anytime</h2>
+        </div>
+        <form action="assets/inc/sendemail.php" class="contact-page-form__form-box contact-form-validated"
+            novalidate="novalidate">
+            <div class="row">
+                <div class="col-xl-6">
+                    <div class="contact-page-form__input-box">
+                        <input type="text" placeholder="Your name" name="name">
+                    </div>
+                </div>
+                <div class="col-xl-6">
+                    <div class="contact-page-form__input-box">
+                        <input type="email" placeholder="Email address" name="email">
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="contact-page-form__input-box text-message-box">
+                        <textarea name="message" placeholder="Write message"></textarea>
+                    </div>
+                    <div class="contact-page-form__btn-box">
+                        <button type="submit" class="thm-btn contact-page-form__btn">Send a message</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+        <div class="result"></div>
+    </div>
+</section>
+<!--Contact Page Form End-->
+
+<!--Google Map Start-->
+<section class="contact-page-google-map">
+    <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4562.753041141002!2d-118.80123790098536!3d34.152323469614075!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80e82469c2162619%3A0xba03efb7998eef6d!2sCostco+Wholesale!5e0!3m2!1sbn!2sbd!4v1562518641290!5m2!1sbn!2sbd"
+        class="google-map__two" allowfullscreen></iframe>
+
+</section>
+<!--Google Map End-->
 
 
 @endsection
