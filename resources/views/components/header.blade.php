f10c4ea4fb5c7719f1314264dc68aeb22a712e8f
insolutes2023 <testinsolutes@gmail.com>
2025-11-11 09:56:52 +0530

    files

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -50,77 +50,26 @@
             <div class="main-menu-two__wrapper-inner">
                 <div class="main-menu-two__left">
                     <div class="main-menu-two__logo">
-                        <a href="index.html"><img src="assets/images/resources/logo-2.png" alt=""></a>
+                        <a href="index.html"><img src="assets/images/resources/logo.webp" alt=""></a>
                     </div>
                     <div class="main-menu-two__main-menu-box">
                         <a href="#" class="mobile-nav__toggler"><i class="fa fa-bars"></i></a>
                         <ul class="main-menu__list">
-                            <li class="dropdown">
-                                <a href="index.html">Home </a>
-                                <ul>
-                                    <li><a href="index.html">Home One</a></li>
-                                    <li><a href="index2.html">Home Two</a></li>
-                                    <li><a href="index3.html">Home Three</a></li>
-                                    <li><a href="index4.html">Home Four</a></li>
-                                    <li><a href="index5.html">Home Five</a></li>
-                                    <li><a href="index6.html">Home Six</a></li>
-                                    <li class="dropdown">
-                                        <a href="#">Header Styles</a>
-                                        <ul>
-                                            <li><a href="index.html">Header One</a></li>
-                                            <li><a href="index2.html">Header Two</a></li>
-                                            <li><a href="index3.html">Header Three</a></li>
-                                            <li><a href="index4.html">Home Four</a></li>
-                                            <li><a href="index5.html">Home Five</a></li>
-                                            <li><a href="index6.html">Home Six</a></li>
-                                        </ul>
-                                    </li>
-                                </ul>
+                            <li>
+                                <a href="{{config('app.url')}}">Home</a>
                             </li>
                             <li>
-                                <a href="about.html">About</a>
+                                <a href="{{route('about-us')}}">About us</a>
                             </li>
-                            <li class="dropdown">
-                                <a href="#">Pages</a>
-                                <ul>
-                                    <li><a href="team.html">Our Team</a></li>
-                                    <li><a href="our-clients.html">Our Clients</a></li>
-                                    <li><a href="faq.html">FAQs</a></li>
-                                </ul>
+
+                            <li>
+                                <a href="{{route('about-us')}}">Our Products</a>
                             </li>
-                            <li class="dropdown">
-                                <a href="#">Services</a>
-                                <ul>
-                                    <li><a href="services-1.html">Services 01</a></li>
-                                    <li><a href="services-2.html">Services 02</a></li>
-                                    <li><a href="services-3.html">Services 03</a></li>
-                                    <li><a href="services-4.html">Services 04</a></li>
-                                    <li><a href="services-5.html">Services 05</a></li>
-                                    <li><a href="services-6.html">Services 06</a></li>
-                                    <li><a href="mechanical-expert.html">Mechanical Expert</a></li>
-                                    <li><a href="repair-welding.html">Repair & Welding</a></li>
-                                    <li><a href="appartment-design.html">Appartment Design</a></li>
-                                    <li><a href="design-build.html">Design & Build</a></li>
-                                    <li><a href="house-renovation.html">House Renovation</a></li>
-                                    <li><a href="building-material.html">Building Material</a></li>
-                                </ul>
+
+                            <li>
+                                <a href="{{route('about-us')}}">Services</a>
                             </li>
-                            <li class="dropdown">
-                                <a href="#">Projects</a>
-                                <ul>
-                                    <li><a href="projects-1.html">Projects 01</a></li>
-                                    <li><a href="projects-2.html">Projects 02</a></li>
-                                    <li><a href="projects-3.html">Projects 03</a></li>
-                                    <li><a href="project-details.html">Project Details</a></li>
-                                </ul>
-                            </li>
-                            <li class="dropdown">
-                                <a href="#">News</a>
-                                <ul>
-                                    <li><a href="news.html">News</a></li>
-                                    <li><a href="news-details.html">News Details</a></li>
-                                </ul>
-                            </li>
+
                             <li>
                                 <a href="contact.html">Contact</a>
                             </li>
@@ -129,12 +78,8 @@
                 </div>
                 <div class="main-menu-two__right">
                     <div class="main-menu-two__search-cart-btn-box">
-                        <div class="main-menu-two__search-box">
-                            <a href="#" class="main-menu-two__search search-toggler icon-magnifying-glass"></a>
-                        </div>
-                        <div class="main-menu-two__cart-box">
-                            <a href="#" class="main-menu-two__cart icon-shopping-cart"></a>
-                        </div>
+
+
                         <div class="main-menu-two__call">
                             <div class="main-menu-two__call-icon">
                                 <span class="icon-phone-ringing"></span>
